baead37ac3f9d5a12bd8aae8dcedc6a2f8fe88e4
Kelvin Kee Kwong Yew <bcs23090011@student.uts.edu.my>
2025-10-21 14:39:09 +0800

    Add project files.

---
+++ Grid_layout/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Grid_layout
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
